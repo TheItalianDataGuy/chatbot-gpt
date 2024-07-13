dc63ee36a45a478b1390456cc8f1263222a73394
Andrew <andreamarella0@gmail.com>
2024-07-13 20:16:33 +0100

    Added the method to send the message and receive the answer from the chatbot

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, \
     QPushButton, QApplication
 import sys
+from backend import Chatbot
+import threading
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
+        """This method creates the GUI app"""
         super().__init__()
+
+        self.chatbot = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -21,7 +26,26 @@ class ChatbotWindow(QMainWindow):
         # Add the button Send
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
         self.show()
+
+    def send_message(self):
+        """This method allows to communicate with the gpt3.5-turbo"""
+        # Extract the user input
+        user_input = self.input_area.text().strip()
+
+        # Add the user input into the chat area
+        self.chat_area.append(f"<p style='color: #333333'>Me: {user_input}</p>")
+        self.input_area.clear()
+
+        # Adding the thread will reduce the latency of the user input in the chat area
+        thread = threading.Thread(target=self.get_bot_response, args=(user_input, ))
+        thread.start()
+
+    def get_bot_response(self, user_input):
+        """This method add the response of the model to the chat area"""
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333'; background-color: #E9E9E9>Chatbot: {response}</p>")
 
 
 # Instantiate the app
